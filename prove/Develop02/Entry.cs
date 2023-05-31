567b60e22ea1104dcc85072311fd41c7fa91c71d
AndreHdz10 <aandreehdz@gmail.com>
2023-05-30 22:32:45 -0600

    Prove Week 02

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,46 @@
+public class Entry
+{
+
+    public string _prompt;
+    public string _reply;
+
+    public DateTime _date;
+
+    public Entry(string prompt, string reply)
+    {
+        //This is utilized while writing a new entry
+        _prompt = prompt;
+        _reply = reply;
+        _date = DateTime.Now;
+    }
+
+    public Entry(string date, string prompt, string reply)
+    {
+        //This is utilized to load an entry from the csv
+        _prompt = prompt;
+        _reply = reply;
+        _date = DateTime.Parse(date);
+    }
+
+    public override string ToString()
+    {
+        //Instead of creating the Display method I preferred to override the ToString
+        string entryFormatted = $"Date: {_date.ToShortDateString()} - Prompt: {_prompt} \n{_reply}";
+        return entryFormatted;
+    }
+
+    public static string RandomPrompt()
+    {
+        //Create static method to utilize even without variable created which makes sense for this
+        Random rd = new Random();
+        int randNum = rd.Next(1, 5);
+        Dictionary<int, string> dictPrompts = new Dictionary<int, string>(){
+            {1, "What would I do better today?"},
+            {2, "What's my best achievement today?"},
+            {3, "What was the best part of my day?"},
+            {4, "Did I try hard enough?"},
+            {5, "How did I see the hand of the Lord in my life today?"}
+        };
+        return dictPrompts[randNum];
+    }
+}
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,42 @@
+using System.IO;
+public class Journal
+{
+
+    public string _name;
+    public List<Entry> _entries = new List<Entry>();
+
+    public void Display()
+    {
+        foreach (Entry entry in _entries)
+        {
+            Console.WriteLine($"{entry}\n");
+        }
+    }
+
+    public void SaveJournal()
+    {
+        string filename = "myFile.csv";
+
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date},{entry._prompt},{entry._reply}"); //save with commas to read it later with LoadJournal to build the objects
+            }
+        }
+        Console.WriteLine("Your prompt/s were saved into a csv file\n");
+    }
+
+    public void LoadJournal()
+    {
+        string filename = "myFile.csv";
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(",");
+            Entry newEntry = new Entry(parts[0], parts[1], parts[2]);
+            Console.WriteLine(newEntry);
+        }
+    }
+}
